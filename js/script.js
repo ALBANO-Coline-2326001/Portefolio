08e461bca68b65a4c36cbac49b681757f0ead885
Albano <coline.albano@etu.univ-amu.fr>
2024-02-07 20:08:29 +0200

    Ajout du respansive

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/* Scrool section */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+/* Menu Hamburger */
+const menuHamburger = document.querySelector('.menu-hamburger')
+const navLinks2 = document.querySelector('.nav-links')
+
+menuHamburger.addEventListener('click', () => {
+    navLinks2.classList.toggle('mobile-menu')
+});
